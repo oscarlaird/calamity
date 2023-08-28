4055c5cde7b2551d0f62b15c0287e9f3114ae9aa
Oscar Laird <olaird25@gmail.com>
2023-08-28 19:00:57 -0400

    runner script

--- calamity_runner.py
+++ calamity_runner.py
@@ -1,5 +1,5 @@
 import sys
 import subprocess
 
-if __name__=="__main__":
-    subprocess.run([sys.executable, '-m', 'calamity'] + sys.argv[1:])
+def main():
+    subprocess.run([sys.executable, '-m', 'calamity_calendar'] + sys.argv[1:])
